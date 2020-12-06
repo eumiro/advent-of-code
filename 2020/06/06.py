f1073f78855b8c1579f357b147c90d4170a410a4
Miroslav Šedivý <6774676+eumiro@users.noreply.github.com>
2020-12-06 07:17:59 +0100

    06 updated

--- 2020/06/06.py
+++ 2020/06/06.py
@@ -1,3 +1,2 @@
-print(sum(len(set(block.replace('\n', ''))) for block in open("input").read().split("\n\n")))
-
-print(sum(len(set.intersection(*[set(line) for line in block.strip().split('\n')])) for block in open("input").read().split("\n\n")))
+print(sum(len(set.union       (*[set(line) for line in block.split()])) for block in open("input").read().split("\n\n")))
+print(sum(len(set.intersection(*[set(line) for line in block.split()])) for block in open("input").read().split("\n\n")))
